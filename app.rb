6b67ed48c609bc43bfdd36f20c0c7ded14a72a3b
RTurney <r.turney93@gmail.com>
2021-04-05 21:10:24 +0100

    Add entry page and entry form

--- app.rb
+++ app.rb
@@ -6,4 +6,9 @@ class Diary < Sinatra::Base
   get '/' do
     erb(:index)
   end
+
+  post '/entry' do
+    @entry = params[:entry]
+    @entry
+  end
 end
--- spec/features/mainPage_spec.rb
+++ spec/features/mainPage_spec.rb
@@ -5,4 +5,11 @@ feature 'main page' do
     visit('/')
     expect(page).to have_content 'Diary App'
   end
+
+  scenario 'a user can add a diary entry' do
+    visit('/')
+    fill_in 'entry', with: 'A new entry'
+    click_button 'Submit'
+    expect(page).to have_content 'A new entry'
+  end
 end
